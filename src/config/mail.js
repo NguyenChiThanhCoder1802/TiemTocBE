7702125e12d7bf1e4c810fd3ad95234eddf5af95
NguyenChiThanhCoder1802 <ngchithanh777@gmail.com>
2026-03-06 13:28:59 +0700

    Change Gmail service to smtp.gmail.com

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 import { env } from "../config/environment.js";
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "smtp.gmail.com",
   auth: {
     user: env.EMAIL_USER,
     pass: env.EMAIL_PASS,
