3a9f0faa6c950807e02a397cb6d2ee935a4822b1
Nguyen Chi Thanh <ngchithanh777@gmail.com>
2026-03-09 00:25:20 +0700

    Chỉnh múi giờ

--- src/utils/booking/generateSlots.js
+++ src/utils/booking/generateSlots.js
@@ -3,11 +3,11 @@ import { BUSINESS_CONFIG } from "../../config/business.config.js";
 export const generateDaySlots = (date) => {
   const slots = [];
 
-  const start = new Date(date);
-  start.setHours(BUSINESS_CONFIG.openHour, 0, 0, 0);
+  const start = new Date(`${date}T00:00:00.000Z`)
+  start.setUTCHours(BUSINESS_CONFIG.openHour - 7, 0, 0, 0)
 
-  const end = new Date(date);
-  end.setHours(BUSINESS_CONFIG.closeHour, 0, 0, 0);
+  const end = new Date(`${date}T00:00:00.000Z`)
+  end.setUTCHours(BUSINESS_CONFIG.closeHour - 7, 0, 0, 0)
 
   while (start < end) {
     slots.push(new Date(start));
